5f6a9f80fe69ef381be81129f3fe5d4d6ae3a638
San Miguel <sanmiguel@sans-macbook-pro.local>
2019-01-28 22:01:33 -0500

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by San Miguel on 1/28/19.
+//  Copyright © 2019 San Miguel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
